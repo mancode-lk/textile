4ee863dd62e75c60c710fda81ebc5a69ae1f0b08
mnmnaveed <44447969+mnmnaveedshadow@users.noreply.github.com>
2025-02-28 12:14:49 +0530

    changes

--- layouts/sidebar.php
+++ layouts/sidebar.php
@@ -15,6 +15,10 @@
         <li class="<?php if($pg_name=='pos_grm.php'){echo('active');} ?>">
     <a href="pos_grm.php"><i class="fas fa-cash-register"></i><span> Bills</span></a>
 </li>
+<li class="<?php if($pg_name=='manage_expenses.php'){echo('active');} ?>">
+    <a href="manage_expenses.php"><i class="fas fa-money-bill-wave"></i><span> Expenses</span></a>
+</li>
+
 
 
         <li class="<?php if($pg_name=='vendorlist.php'){echo('active');} ?>" >
